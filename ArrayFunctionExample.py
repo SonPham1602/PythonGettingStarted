3b9c9290b2499e7bdea24e0a9bf10411559760d1
SonPham <phamngocsonl13.cla@gmail.com>
2019-07-22 10:23:25 +0700

    Update Example

--- ArrayFunctionExample.py
+++ ArrayFunctionExample.py
@@ -1,17 +1,25 @@
-#We are able to create a array of int
-#This is empty array
-array=[]
-#Add a new data in array
+# We are able to create a array of int
+# This is empty array
+array = []
+# Add a new data in array
 array.append(1)
 array.append(2)
 array.append(3)
 array.append(4)
 array.append(5)
-#Length of array
-print("Length of array is",len(array))
+# Length of array
+print("Length of array is", len(array))
 print(array)
-#delete one item in array
+# delete one item in array
 array.pop()
 array.remove(1)
-print(array) 
+print(array)
+# reverse array
+array.reverse()
+print("Array after reverse:", array)
+# Create another array
+array2 = [9, 8, 7, 6, 5]
+print("Array2 :",array2)
+array3 = array + array2
+print("Array3 is copy array :",array3)
 
--- Example.py
+++ Example.py
@@ -7,7 +7,7 @@ for i in range(1,9):
     result2 = result2*i
 
 print(result2)
-
+# Bai2
 # Viết một chương trình có thể tính giai thừa của một số cho trước. 
 # Kết quả được in thành chuỗi trên một dòng, phân tách bởi dấu phẩy. 
 # Ví dụ, số cho trước là 8 thì kết quả đầu ra phải là 40320.
@@ -32,5 +32,48 @@ array.sort()
 print("After ascending order :",array)
 array.sort(reverse=True)
 print("After descending order :",array)
+#Bai 3 
+#Với số nguyên n nhất định, hãy viết chương trình để tạo ra một dictionary chứa (i, i*i) 
+# như là số nguyên từ 1 đến n (bao gồm cả 1 và n) sau đó in ra dictionary này. 
+# Ví dụ: Giả sử số n là 8 thì đầu ra sẽ là: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}.
+n = int(input("Nhap vao mot so:"))
+d=dict()
+for i in range(1,n+1):
+    d[i] = i*i
+ 
+print(d)
+#Bai 4
+#Viết chương trình chấp nhận một chuỗi số, phân tách bằng dấu phẩy từ giao diện điều khiển,
+# tạo ra một danh sách và một tuple chứa mọi số.
+# Ví dụ: Đầu vào được cung cấp là 34,67,55,33,12,98 thì đầu ra là:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+result4=input("Nhap vao gia tri :")
+l=result4.split(",")
+t=tuple(l)
+print(l)
+print(t)
+#Bai 5
+#Định nghĩa một class có ít nhất 2 method: 
+#getString: để nhận một chuỗi do người dùng nhập vào từ giao diện điều khiển.
+#printString: in chuỗi vừa nhập sang chữ hoa. 
+#Thêm vào các hàm hiểm tra đơn giản để kiểm tra method của class.
+#Ví dụ: Chuỗi nhập vào là quantrimang.com thì đầu ra phải là: QUANTRIMANG.COM
+
+class InputOutputString(object):
+    def __init__(self, *args, **kwargs):
+        self.Str=""
+        return super().__init__(*args, **kwargs)
+    def getString(self):
+        self.Str = input("Nhap chuoi:")
+    
+    def printString(self):
+        if not self.Str:
+            print("Chuoi rong")
+        
+        print(self.Str)
+
+result5 = InputOutputString()
+result5.printString()
 
 
