b564b29cfff8a2f9820a5a9966d556150bc5de3e
SonPham <phamngocsonl13.cla@gmail.com>
2019-07-26 14:45:30 +0700

    Update file and class Example

---
+++ ClassExample.py
@@ -0,0 +1,22 @@
+class Person:
+    #Thuoc tinh
+    name = ""
+    age = 22
+    male =True
+    #Phuong thuc
+    def setName(self,name):
+        self.name = name
+    
+    def getName(self):
+        return self.name
+
+    def setAge(self,age):
+        self.age = age
+    
+    def getAge(self):
+        return self.age
+
+    
+
+
+
---
+++ FileExample.py
@@ -0,0 +1,6 @@
+# Để đọc ghi file trong python ta sử dụng hàm open()
+# với cách tham số chuyên vào như sau
+# open(filePath,mode, buffer)
+# filePath: đường dẫn file
+# mode: chế độ độc file
+# buffer:
